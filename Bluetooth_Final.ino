10462c22cbd5ccc2d9fcba1995aeca461aa4e6a7
sahil811 <sahil8sharma8@gmail.com>
2017-04-12 21:53:42 +0530

    Add files via upload

---
+++ Bluetooth_Final.ino
@@ -0,0 +1,28 @@
+#include<SoftwareSerial.h>
+SoftwareSerial BT(10,11);
+int btread;
+void setup()
+{
+  pinMode(6,OUTPUT);
+  digitalWrite(6,LOW);
+  pinMode(7,OUTPUT);
+  digitalWrite(7,HIGH);
+  Serial.begin(9600);
+  BT.begin(9600);
+  Serial.println("Bluetooth AT command mode");
+}
+void loop()
+{
+  if(BT.available())
+   {
+     btread=BT.read();
+    Serial.write(btread);
+    if(btread=='1')
+    digitalWrite(6,HIGH);
+    else if(btread=='0')
+    digitalWrite(6,LOW);
+   } 
+  if(Serial.available())
+    BT.write(Serial.read());
+}
+
